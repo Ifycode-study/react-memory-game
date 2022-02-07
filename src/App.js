d148ddf58d2ec3d02eabea579d24333de2f1f6af
Ifycode <marychord@gmail.com>
2022-02-07 07:19:40 +0100

    Learning: Setting up & shuffling cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 
+const cardImages = [
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/potion-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/sword-1.png" }
+]
+
 function App() {
+  const [ cards, setCards ] = useState([]);
+  const [ turns, setTurns ] = useState(0);
+
+  const cardsShuffle = () => {
+    const duplicatedCards = [ ...cardImages, ...cardImages];
+    const shuffleCards = duplicatedCards
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }));
+  
+      setCards(shuffleCards);
+      setTurns(0);
+  }
+
   return (
     <div className="App">
       <h1>Memory game</h1>
-      <button>Play</button>
+      <button onClick={cardsShuffle}>New Game</button>
     </div>
   );
 }
